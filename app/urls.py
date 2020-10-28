395d9f99cc85b65d77dd04a80a7258c473137535
pranali kambli <pranalikambli2013@.com>
2020-10-28 16:06:39 +0530

    url bug resolved

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('basic_product/', views.BasicProductView.as_view(), name="basic_product_view"),
-    path('crispy_product/', views.TweakProductView.as_view(), name="tweak_product_view"),
+    path('tweak_product/', views.TweakProductView.as_view(), name="tweak_product_view"),
     path('customize_tweak_product/', views.CustomizeTweakProductView.as_view(), name="customize_tweak_product_view"),
 ]
